de75039885e02523e5ea7646e8d8b455bbd6f141
wemmm <jenny.wem@gmail.com>
2017-08-22 14:59:47 +0100

    User can now use interface to select a specific book from a shelf

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
   var shelf = new Shelf;
-})
 
-$("#submit").on('click', function() {
-  shelf.getSpecificBook($("#bookinput").val())
-  $('#booktitle').text(shelf.specificBook.bookTitle)
-  $('#page1').text(shelf.specificBook.pages[0].page)
-  $('#page2').text(shelf.specificBook.pages[1].page)
+  $("#submitbook").on('click', function() {
+    shelf.getSpecificBook($("#bookinput").val())
+    $('#booktitle').text(shelf.specificBook.bookTitle)
+    $('#page1').text(shelf.specificBook.pages[0].page)
+    $('#page2').text(shelf.specificBook.pages[1].page)
+  })
+
+  $("#submitpage").on('click', function() {
+    shelf.getSpecificPages($("#pageinput").val())
+    $('#page1').text(shelf.specificBook.pages[0].page)
+    $('#page2').text(shelf.specificBook.pages[1].page)
+  })
+
 })
