58eb67775e7b2a30f228a87086642597d6a35bc6
wemmm <jenny.wem@gmail.com>
2017-09-14 13:27:34 +0100

    Tweaks random character generation

--- public/javascript/page.js
+++ public/javascript/page.js
@@ -1,5 +1,5 @@
 function Page() {
-  this.possibleLetters = ['a', 'a', 'a', 'b', 'c', 'd', 'e', 'e', 'e', 'f', 'g', 'h', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'n', 'o', 'p', 'q', 'r', 'r', 's', 's', 't', 't', 't', 'u', 'v', 'w', 'x', 'y', 'z', ', ', '. ', ' ', ' ', ' ',]
+  this.possibleLetters = ['a', 'a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd', 'e', 'e', 'e', 'e', 'f', 'f', 'g', 'g', 'h', 'h', 'h', 'i', 'i', 'j', 'j', 'k', 'k', 'l', 'l', 'm', 'm', 'n', 'n', 'n', 'o', 'o', 'p', 'p', 'q', 'r', 'r', 'r', 's', 's', 's', 't', 't', 't', 't', 'u', 'u', 'v', 'w', 'x', 'y', 'z', ', ', '. ', ' ', ' ', ' ', ' ']
   this.line = []
   this.page = []
   this.generatePage()
